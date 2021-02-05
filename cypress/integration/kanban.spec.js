1b4462ba844b8cc29bd3b8c6d9058cdd25cb20db
yujo <yujo@student.42seoul.kr>
2021-02-05 22:46:47 +0900

    test: cypress init

---
+++ cypress/integration/kanban.spec.js
@@ -0,0 +1,5 @@
+describe('kanban', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5500/kanban.html');
+  });
+});
---
+++ cypress/integration/team.spec.js
@@ -0,0 +1,5 @@
+describe('team', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5500/index.html');
+  });
+});
